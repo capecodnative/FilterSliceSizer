f97ae16b19fec3a1ea8f692211a35450f918a296
Dan Ohnemus <42098714+capecodnative@users.noreply.github.com>
2024-09-04 22:05:32 -0400

    Update CalculateWedgesAllFilesInFolder.m

--- CalculateWedgesAllFilesInFolder.m
+++ CalculateWedgesAllFilesInFolder.m
@@ -1,6 +1,6 @@
 % A function that goes through all files in a directory specified
 % calls AngleFinderSliceSizer on each, and saves the struct output
-% of each to a .csv named after the file as filename_wedges.csv
+% of each to a .txt named after the file as filename_wedges.txt
 
 function CalculateWedgesAllFilesInFolder(folderPath)
     % Get a list of all jpg files in the specified folder
